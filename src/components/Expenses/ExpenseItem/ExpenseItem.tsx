9810f1e6efc6bdbfea23ca62133de4b7d8f96fbb
Anna Zanovskaya <zanovskaya@mercurydevelopment.com>
2023-06-06 18:34:42 +0300

    Add simple state

--- src/components/Expenses/ExpenseItem/ExpenseItem.tsx
+++ src/components/Expenses/ExpenseItem/ExpenseItem.tsx
@@ -2,16 +2,26 @@ import './ExpenseItem.css';
 import Expense from "../../../types/expense";
 import ExpenseDate from "../ExpenseDate";
 import Card from "../../UI/Card";
+import {useState} from "react";
 
-const ExpenseItem = (props: Expense) => (
-  <Card className="expense-item">
-    <ExpenseDate date={props.date}/>
-    <div className="expense-item__description">
-      <h2>{props.title}</h2>
-      <div className="expense-item__price">${props.amount}</div>
-    </div>
-  </Card>
-);
+const ExpenseItem = (props: Expense) => {
+
+  const [title, setTitle] = useState<string>(props.title);
+  const clickHandler = () => {
+    setTitle('Updated');
+  }
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}/>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Update Title</button>
+    </Card>
+  );
+}
 
 
 export default ExpenseItem;
