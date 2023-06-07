ecdaa42dcf285eca3fbf8374e7078def8835d79f
Anna Zanovskaya <zanovskaya@mercurydevelopment.com>
2023-06-07 18:40:23 +0300

    Add state for expenses list

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import NewExpense from './components/NewExpense';
 import {Expense} from './types/expense';
-import ExpenseList from "./components/Expenses/ExpenseList";
+import ExpenseList from './components/Expenses/ExpenseList';
 
-const App = () =>{
-  const expenses: Expense[] = [
+const App = () => {
+
+  const [expenses, setExpensess] = useState<Expense[]>([
     {
       id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12)},
     {
       id: 'e3',
       title: 'Car Insurance',
@@ -25,10 +26,33 @@ const App = () =>{
       amount: 450,
       date: new Date(2021, 5, 12),
     },
-  ];
+  ]);
+  // const expenses: Expense[] = [
+  //   {
+  //     id: 'e1',
+  //     title: 'Toilet Paper',
+  //     amount: 94.12,
+  //     date: new Date(2020, 7, 14),
+  //   },
+  //   {id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12)},
+  //   {
+  //     id: 'e3',
+  //     title: 'Car Insurance',
+  //     amount: 294.67,
+  //     date: new Date(2021, 2, 28),
+  //   },
+  //   {
+  //     id: 'e4',
+  //     title: 'New Desk (Wooden)',
+  //     amount: 450,
+  //     date: new Date(2021, 5, 12),
+  //   },
+  // ];
 
   const onAddNewExpense = (expenseData: Expense) => {
-    console.log(expenseData)
+    console.log(expenseData);
+    // expenses.push(expenseData);
+    setExpensess((prevState) => [...prevState, expenseData])
   }
 
   return (
